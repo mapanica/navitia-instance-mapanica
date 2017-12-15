4e00d06ff55980817f6f2709367cf21e563c0662
Nico Alt <nicoalt@posteo.org>
2017-12-15 13:52:47 -0600

    Improve script

--- update-esteli.sh
+++ update-esteli.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 # Put this script into the data folder of https://github.com/mapanica/navitia-instance-mapanica
 set -e
+cd /srv/navitia-docker-compose-master/data/
 echo "Downloading GTFS of Estelí..."
-curl -O https://datos.mapanica.net/ni-esteli-gtfs.zip
+curl -O https://datos.mapanica.net/ni-esteli-gtfs.zip -z ni-esteli-gtfs.zip.old
+if [ ! -f ni-esteli-gtfs.zip ]; then
+	cp ni-esteli-gtfs.zip.old ni-esteli-gtfs.zip
+fi
 echo "Checking if GTFS has changed..."
 if ! cmp --silent ni-esteli-gtfs.zip ni-esteli-gtfs.zip.old; then
 	echo "Deploying to navitia..."
--- update-managua.sh
+++ update-managua.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 # Put this script into the data folder of https://github.com/mapanica/navitia-instance-mapanica
 set -e
+cd /srv/navitia-docker-compose-master/data/
 echo "Downloading GTFS of Managua..."
-curl -O https://datos.mapanica.net/ni-managua-gtfs.zip
+curl -O https://datos.mapanica.net/ni-managua-gtfs.zip -z ni-managua-gtfs.zip.old
+if [ ! -f ni-managua-gtfs.zip ]; then
+	cp ni-managua-gtfs.zip.old ni-managua-gtfs.zip
+fi
 echo "Checking if GTFS has changed..."
 if ! cmp --silent ni-managua-gtfs.zip ni-managua-gtfs.zip.old; then
 	echo "Deploying to navitia..."
